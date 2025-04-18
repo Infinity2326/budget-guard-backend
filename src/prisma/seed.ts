2fdec5a91bdc15cd39e62e9eb59d5bcb2f373a27
infinity2326 <dima.gorbachev2002@mail.ru>
2025-04-19 00:29:03 +0300

    fix: update dependencies & seed prisma default categories

---
+++ src/prisma/seed.ts
@@ -0,0 +1,36 @@
+import { PrismaClient } from '@prisma/client'
+import * as dotenv from 'dotenv'
+
+dotenv.config()
+const prisma = new PrismaClient()
+
+const incomeId = process.env.DEFAULT_INCOME_ID!
+const expenseId = process.env.DEFAULT_EXPENSE_ID!
+
+async function main() {
+  await prisma.category.create({
+    data: {
+      id: incomeId,
+      name: 'Default Income',
+      type: 'INCOME',
+    },
+  })
+
+  await prisma.category.create({
+    data: {
+      id: expenseId,
+      name: 'Default Expense',
+      type: 'EXPENSE',
+    },
+  })
+}
+
+main()
+  .then(async () => {
+    await prisma.$disconnect()
+  })
+  .catch(async (e) => {
+    console.error(e)
+    await prisma.$disconnect()
+    process.exit(1)
+  })
