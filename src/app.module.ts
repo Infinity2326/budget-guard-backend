ef02f8871e29cbc9b844c93f630ff942504d6388
infinity2326 <dima.gorbachev2002@mail.ru>
2025-05-24 19:45:34 +0300

    fix: remove app module & controller

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,4 @@
 import { Module } from '@nestjs/common'
-import { AppController } from './app.controller'
-import { AppService } from './app.service'
 import { ConfigModule } from '@nestjs/config'
 import { PrismaModule } from './prisma/prisma.module'
 import { RedisModule } from './redis/redis.module'
@@ -21,7 +19,7 @@ import { CategoryModule } from './category/category.module'
     TransactionModule,
     CategoryModule,
   ],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
